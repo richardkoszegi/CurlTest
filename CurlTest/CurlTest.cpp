09c166af09fbb595026ce22c02c02b8e3214ecc5
richardkoszegi <richard.koszegi95@gmail.com>
2016-03-17 22:33:06 +0100

    Stringeket kicsereltem char*-okra, igy mar tudok kommunikalni a szerverrel.

--- CurlTest/CurlTest.cpp
+++ CurlTest/CurlTest.cpp
@@ -8,25 +8,26 @@
 
 class OneDriveHandler {
 private:
-	std::string clientId = "";
+	char* clientId = "";
 public:
 	void LogIn() {
 		CURL* logincurl = curl_easy_init();
 		if (logincurl) {
 			CURLcode res;
-			std::string scope = "onedrive.readwrite";
-			std::string loginaddress = "https://login.live.com/oauth20_authorize.srf?client_id="+clientId+"&scope="+scope+"&response_type=token&redirect_uri= https://login.live.com/oauth20_desktop.srf";
-			std::cout << loginaddress+"\n";
-			//curl_easy_setopt(logincurl, CURLOPT_URL, loginaddress);
-			//curl_easy_setopt(logincurl, CURLOPT_HEADER, 1);
-			//curl_easy_setopt(logincurl, CURLOPT_RETURNTRANSFER, 1);
-			//curl_easy_setopt(logincurl, CURLOPT_HTTPGET, 1);
-			//curl_easy_setopt(logincurl, CURLOPT_DNS_USE_GLOBAL_CACHE, false);
-			//curl_easy_setopt(logincurl, CURLOPT_DNS_CACHE_TIMEOUT, 2);
+
+			char* scope = "onedrive.readwrite";
+			char loginaddress[2000];
+			strcpy_s(loginaddress, "https://login.live.com/oauth20_authorize.srf?client_id=");
+			strcat_s(loginaddress, clientId);
+			strcat_s(loginaddress,"&scope=" );
+			strcat_s(loginaddress, scope);
+			strcat_s(loginaddress, "&response_type=token&redirect_uri=https://login.live.com/oauth20_desktop.srf");
+
+			curl_easy_setopt(logincurl, CURLOPT_URL, loginaddress);
+			curl_easy_setopt(logincurl, CURLOPT_HTTPGET, 1L);
 
 			res = curl_easy_perform(logincurl);
 			curl_easy_cleanup(logincurl);
-
 
 			if (res != CURLE_OK)
 				fprintf(stderr, "curl_easy_perform() failed: %s\n",
@@ -39,11 +40,17 @@ public:
 	}
 
 	void LogOut() {
+		std::cout << "[LOG] Logout begin\n";
 		CURL* logoutcurl = curl_easy_init();
 		if (logoutcurl) {
 			CURLcode res;
-			std::string loginaddress = "https://login.live.com/oauth20_logout.srf?client_id=" + clientId + "&redirect_uri= https://login.live.com/oauth20_desktop.srf";
-			curl_easy_setopt(logoutcurl, CURLOPT_URL, loginaddress);
+
+			char logoutaddress[2000];
+			strcpy_s(logoutaddress, "https://login.live.com/oauth20_logout.srf?client_id=");
+			strcat_s(logoutaddress, clientId);
+			strcat_s(logoutaddress,"&redirect_uri=https://login.live.com/oauth20_desktop.srf" );
+
+			curl_easy_setopt(logoutcurl, CURLOPT_URL, logoutaddress);
 			res = curl_easy_perform(logoutcurl);
 			curl_easy_cleanup(logoutcurl);
 
@@ -60,25 +67,6 @@ public:
 	void UploadFiel() {}
 };
 
-void test() {
-	CURL* curl = curl_easy_init();
-	if (curl) {
-		CURLcode res;
-		curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
-		res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-
-		if (res != CURLE_OK)
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-				curl_easy_strerror(res));
-		else {
-			std::cout << res;
-			std::cout << "\n[LOG] Logout Done\n";
-		}
-	}
-}
-
-
 int main()
 {
 	curl_global_init(CURL_GLOBAL_ALL);
@@ -87,7 +75,6 @@ int main()
 	odh->LogIn();
 
 	odh->LogOut();
-	//test();
 
 	std::cin.get();
 
